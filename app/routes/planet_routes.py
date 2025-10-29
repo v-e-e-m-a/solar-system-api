8385ddcb57ec191949c876f86d50c70e13b75578
Veema Jhagru <veemajhagru@gmail.com>
2025-10-29 14:28:55 -0400

    Completed wave 4 and 5

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.planet import Planet
 from ..db import db
 
@@ -24,10 +24,23 @@ def create_planet():
 
     return response, 201
 
+# 1. ... to get a list of `planet`s, restricted to those with a match in the `description`, so that I can find a planet by a partial description.
+# 2. 
 
 @planets_bp.get("")
 def get_all_planet():
-    query = db.select(Planet).order_by(Planet.id)
+    name_param = request.args.get("name")
+    description_param = request.args.get("description")
+    query = db.select(Planet)
+
+    if name_param:
+        query = query.where(Planet.name == name_param)
+    
+    if description_param:
+        query = query.where(Planet.description.ilike(f"%{description_param}%"))
+
+    query = query.order_by(Planet.id)
+
     planets = db.session.scalars(query)
 
     result_list = []
@@ -41,6 +54,9 @@ def get_all_planet():
         ))
     
     return result_list
+
+# Wave 04
+# 1. ...to get one existing `planet`, so that I can see the `id`, `name`, `description`, and other data of the `planet`.
 
 @planets_bp.get("/<id>")
 def get_planet(id):
@@ -71,3 +87,25 @@ def validate_planet(id):
 
     response = {"message": f"Planet id ({id}) is not found."}
     abort(make_response(response, 404))
+
+# 2. ... with valid planet data to update one existing `planet` and get a success response, so that I know the API updated the `planet` data.
+@planets_bp.put("/<id>")
+def update_planet(id):
+    planet = validate_planet(id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.radius = request_body["radius"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json") 
+
+# 3. ... to delete one existing `planet` and get a success response, so that I know the API deleted the `planet` data..
+@planets_bp.delete("/<id>")
+def delete_planet(id):
+    planet = validate_planet(id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
